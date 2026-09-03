ec7182aec5b7a5b3069c1848b606e8413cff2070
Kamijoli <kamijouexe@gmail.com>
2026-09-03 23:04:34 +0500

    Изменение кон

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -69,14 +69,14 @@ export default function Contacts() {
 
           <div className="contactList">
             <a className="contactItem" href="tel:+76990348003">
-              <span className="contactLabels">Сизенёв Алексей Евгеньевич</span>
+              <span className="contactLabels">Алексей Евгеньевич</span>
               <span className="contactLabel">Отдел продаж / работа с клиентами</span>
               <span className="contactValue">+7 915 06 122 11</span>
             </a>
 
           <div className="contactList">
             <a className="contactItem" href="tel:+76990348003">
-              <span className="contactLabels">Бойко Андрей Сергеевич</span>
+              <span className="contactLabels">Андрей Сергеевич</span>
               <span className="contactLabel">Руководитель производства</span>
               <span className="contactValue">+7 951 447 20 47</span>
             </a>
